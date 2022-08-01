2cc68ceb4cf81a2d111443797479e6c1a046e84f
Ishwor-Shrestha <ishworisgod@gmail.com>
2022-08-01 22:25:00 +0545

    Simplify Execute in root.go

--- cmd/root.go
+++ cmd/root.go
@@ -6,8 +6,8 @@ package cmd
 
 import (
 	"os"
-
 	"github.com/spf13/cobra"
+	"github.com/khalti/ksi/util"
 )
 
 
@@ -22,10 +22,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	util.HandleError(rootCmd.Execute())
 }
 
 func init() {
