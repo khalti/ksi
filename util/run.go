61011168e3ec3e655c046b382b3baa9619a234d2
Ishwor-Shrestha <ishworisgod@gmail.com>
2022-08-02 01:43:20 +0545

    Add run.go util to execute shell commands

---
+++ util/run.go
@@ -0,0 +1,13 @@
+package util
+
+import "os/exec"
+
+// Executes a shell command and returns the output as a string
+func Run(name string, args ...string) (string, error){
+	cmd := exec.Command(name, args...)
+	stdOut, err := cmd.Output()
+
+	HandleError(err)
+
+	return string(stdOut), nil
+}
