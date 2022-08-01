64d8aa4015737d09ce3f7317fe51b9674d4daed8
Ishwor-Shrestha <ishworisgod@gmail.com>
2022-08-01 22:25:38 +0545

    Cleanup init from root.go

--- cmd/root.go
+++ cmd/root.go
@@ -5,18 +5,16 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"os"
-	"github.com/spf13/cobra"
 	"github.com/khalti/ksi/util"
+	"github.com/spf13/cobra"
+	"os"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ksi",
 	Short: "Use KSI",
-	Long: `Use KSI`,
+	Long:  `Use KSI`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -26,15 +24,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ksi.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
