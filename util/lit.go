790c644f9ebb9bf3bb3140d996d51ef0fd9d1ed6
Ishwor-Shrestha <ishworisgod@gmail.com>
2022-08-02 18:06:09 +0545

    Add new Trim function to trim all passed characters from a string

--- util/lit.go
+++ util/lit.go
@@ -1,8 +1,18 @@
 package util
 
-import(
+import (
 	"fmt"
+	"strings"
 )
+
+// Trims trailing characters from the passed string
+func Trim(s string, a ...string)string{
+	trimmedOutput := s
+	for _, arg:= range a{
+		trimmedOutput = strings.Trim(trimmedOutput, arg)
+	}
+	return trimmedOutput
+}
 
 // Prints formatted strings
 func Print(format string, a ...any)  {
