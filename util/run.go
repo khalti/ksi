9609cf16f9cec80d8a31afecb41c7777b50d3eb7
Ishwor-Shrestha <ishworisgod@gmail.com>
2022-08-02 18:07:39 +0545

    Fix an issue where Run did not execute shell commands and instead threw an error

--- util/run.go
+++ util/run.go
@@ -3,7 +3,7 @@ package util
 import "os/exec"
 
 // Executes a shell command and returns the output as a string
-func Run(name string, args ...string) string{
+func Run(name string, args ...string) string {
 	cmd := exec.Command(name, args...)
 	stdOut, err := cmd.Output()
 
